d0d8266b50aa26bb37200962abb892b27303ba5b
originalmahim <tareqazizmahim@gmail.com>
2023-10-06 22:25:24 +0600

    Successfully Created AUTH provider for all roots and initialize firebaze with website for login and createa account and sign up

--- src/Components/AuthProvider/AuthProvider.jsx
+++ src/Components/AuthProvider/AuthProvider.jsx
@@ -1,21 +1,33 @@
 import { createContext } from "react";
 import PropTypes from 'prop-types';
-
-
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import app from "../Firebase/firebase.config";
 
 
 
           //Created ContexApi - Fob Objerving user behaivior
           const WholewebsiteContex = createContext(null)
-          
+          const auth = getAuth(app)
           
           
           const AuthProvider = ({children}) => {
 
-          
+          const CreateUser = (email,password) => {
+               return  createUserWithEmailAndPassword(auth,email,password)    
+          }
+
+          const Login = (email,password) => {
+          return signInWithEmailAndPassword(auth,email,password)
+          }
+
+          const SignOut = () => {
+          signOut(auth)
+          }
 
           const userinformation = {
-                    
+                    CreateUser,
+                    Login,
+                    SignOut
           }
 
           return (
--- src/Components/Firebase/firebase.config.js
+++ src/Components/Firebase/firebase.config.js
@@ -1,1 +1,14 @@
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAprf9JwmX_xX8-hCg8cuCfr15WguGjWFY",
+  authDomain: "assignment-project-nine.firebaseapp.com",
+  projectId: "assignment-project-nine",
+  storageBucket: "assignment-project-nine.appspot.com",
+  messagingSenderId: "744304167500",
+  appId: "1:744304167500:web:31b67d8228f8be8586c2e9"
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
